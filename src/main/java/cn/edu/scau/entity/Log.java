f5bf88bcd35be417886ad21795906a160ec0ae02
alan <qiunan_alan@163.com>
2018-07-25 14:49:18 +0800

    实现日志管理

--- src/main/java/cn/edu/scau/entity/Log.java
+++ src/main/java/cn/edu/scau/entity/Log.java
@@ -1,38 +1,29 @@
 package cn.edu.scau.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class Log {
+
     private Integer id;
 
     private String name;
 
     private Integer type;
 
-    private String url;
-
-    private String requestType;
-
-    private String requestParam;
-
     private String user;
 
     private String ip;
 
-    private Integer time;
-
     private Date createDate;
 
-    public Log(Integer id, String name, Integer type, String url, String requestType, String requestParam, String user, String ip, Integer time, Date createDate) {
+    public Log(Integer id, String name, Integer type, String user, String ip, Date createDate) {
         this.id = id;
         this.name = name;
         this.type = type;
-        this.url = url;
-        this.requestType = requestType;
-        this.requestParam = requestParam;
         this.user = user;
         this.ip = ip;
-        this.time = time;
         this.createDate = createDate;
     }
 
@@ -64,30 +55,6 @@ public class Log {
         this.type = type;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url == null ? null : url.trim();
-    }
-
-    public String getRequestType() {
-        return requestType;
-    }
-
-    public void setRequestType(String requestType) {
-        this.requestType = requestType == null ? null : requestType.trim();
-    }
-
-    public String getRequestParam() {
-        return requestParam;
-    }
-
-    public void setRequestParam(String requestParam) {
-        this.requestParam = requestParam == null ? null : requestParam.trim();
-    }
-
     public String getUser() {
         return user;
     }
@@ -104,14 +71,7 @@ public class Log {
         this.ip = ip == null ? null : ip.trim();
     }
 
-    public Integer getTime() {
-        return time;
-    }
-
-    public void setTime(Integer time) {
-        this.time = time;
-    }
-
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     public Date getCreateDate() {
         return createDate;
     }
