74985473c3abf0ff43b683196d4162d4c0d49fe4
MrJiaDong <13602802954@sina.cn>
2018-07-27 17:40:19 +0800

    商品的增删查改，上架下架完成。

---
+++ src/main/java/cn/edu/scau/component/Info.java
@@ -0,0 +1,43 @@
+package cn.edu.scau.component;
+
+import cn.edu.scau.dao.ProductMapper;
+import cn.edu.scau.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import cn.edu.scau.dao.ProductMapper;
+import org.springframework.stereotype.Controller;
+
+@Component
+public class Info<T> {
+
+    private String success;
+    private String error;
+    private T data;
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess() {
+        this.success = "查询成功";
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
