60d60898b381810c7810e15fd781e6ef3ae058ad
alan <qiunan_alan@163.com>
2018-08-01 11:24:19 +0800

    完善用户登录拦截功能

--- src/main/java/cn/edu/scau/interceptor/LoginInterceptor.java
+++ src/main/java/cn/edu/scau/interceptor/LoginInterceptor.java
@@ -14,6 +14,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     public static final String USER_INFO_KEY = "username";
     private List<String> excludeUrl;    //不拦截的url请求
+    private List<String> staticResource;    //静态资源
 
     public List<String> getExcludeUrl() {
         return excludeUrl;
@@ -23,12 +24,24 @@ public class LoginInterceptor implements HandlerInterceptor {
         this.excludeUrl = excludeUrl;
     }
 
+    public List<String> getStaticResource() {
+        return staticResource;
+    }
+
+    public void setStaticResource(List<String> staticResource) {
+        this.staticResource = staticResource;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         //获取Session对象
         HttpSession session = httpServletRequest.getSession();
         //获取请求的url地址
         String requestURL = httpServletRequest.getRequestURL().toString();
+        //判断是否为静态资源
+        if (isStaticResource(requestURL)) {
+            return true;
+        }
         //如果该url是不拦截的url，则不拦截
         if (excludeUrl != null && excludeUrl.contains(requestURL)) {
             return true;
@@ -51,4 +64,18 @@ public class LoginInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
 
     }
+
+    private boolean isStaticResource(String url) {
+        //截取资源
+        String[] paths = url.split("/");
+        if (paths != null) {
+            for (int i = 0; i < paths.length; i++) {
+                if (staticResource.contains(paths[i])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
